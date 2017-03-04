e284749c91ab89e9aab7b2fb25de5f04d9b33e42
let42 <alberto@gucciardi.xyz>
2017-03-04 13:47:52 +0100

    Created the header and the identation of dispenser

---
+++ src/dispenser.cc
@@ -0,0 +1,1 @@
+#include "cola-machine.hh"
---
+++ src/dispenser.hh
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+//Creating the guards
+#ifdef _COCA_COLA_H_
+#define _COCA_COLA_H_
+
+bool theresMoneyBack( );
+bool isEmpty( );
+bool releaseTheProduct( );
+double calculateMoneyBack( );
+int checkStorage( );
+void fillStorage( );
+
+
+#endif
